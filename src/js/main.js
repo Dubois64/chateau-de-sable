94ae797f6e0fcf49d0e77ec1e9d58f009be5020b
dubois64 <constance.dubois@studentfr.ch>
2025-11-12 15:44:12 +0100

    premières animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,23 @@
 import { gsap } from "gsap";
+gsap.to(".header-icone", {
+  y: 150,
+  duration: 1.5,
+  yoyo: true,
+  repeat: -1,
+  ease: "power1.inOut",
+});
+
+gsap.utils.toArray(".experience-bulle-1").forEach((el) => {
+  gsap.from(el, {
+    opacity: 0,
+    y: 30,
+    duration: 3,
+    scrollTrigger: {
+      trigger: el,
+      start: "top 95%", // quand l'élément entre dans la vue
+      toggleActions: "play none none none",
+    },
+  });
+});
+
+ScrollTrigger.refresh();
